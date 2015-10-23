5fdb07d2228fd03a0541e721100670963b5bb13c
Suso <suso@tuenti.com>
2015-10-23 14:26:48 +0000

    Upload the long version to WebDAV

--- upload-latest.sh
+++ upload-latest.sh
@@ -2,4 +2,4 @@
 
 filename=`date +%Y%m%d_%H%M%S`.mp4
 
-curl -s -T /tmp/replay/replay.mp4 http://10.0.11.53:8080/foosball/$filename -o /dev/null
+curl -s -T /tmp/replay/replay_long.mp4 http://10.0.11.53:8080/foosball/$filename -o /dev/null
