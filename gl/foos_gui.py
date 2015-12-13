818353421f4ff697d7eb9881d4e10107caca043c
Stefan Wehner <stefan.wehner@gmail.com>
2015-12-13 23:06:05 +0100

    Reuse textures of all counters

--- gl/foos_gui.py
+++ gl/foos_gui.py
@@ -41,9 +41,13 @@ def mangleDisplay(display):
 
 
 class Counter():
+    textures = None
     def __init__(self, value, shader, **kwargs):
+        if not Counter.textures:
+            Counter.textures = [pi3d.Texture("numbers/%d.png" % i)
+                                   for i in range(0, 10)]
         self.value = value
-        self.numbers = [pi3d.ImageSprite("numbers/%d.png" % i, shader, **kwargs)
+        self.numbers = [pi3d.ImageSprite(Counter.textures[i], shader, **kwargs)
                         for i in range(0, 10)]
         self.anim_start = None
         self.speed = 5
@@ -107,12 +111,10 @@ class Gui():
         self.bg = pi3d.ImageSprite("foosball.jpg", flat, w=1920, h=1080, z=10)
         self.yellow = pi3d.ImageSprite("yellow.jpg", flat, x=-400, y=200, w=300.0, h=300.0, z=5.0)
         self.black = pi3d.ImageSprite("black.jpg", flat, x=400, y=200, w=300.0, h=300.0, z=5.0)
-
         font = pi3d.Font("UbuntuMono-B.ttf", (0, 0, 0, 255), font_size=60)
         self.goal_time = pi3d.String(font=font, string=self.__get_time_since_last_goal(), is_3d=False, y=400, z=6.0)
         self.goal_time.set_shader(flat)
 
-        # TODO: reuse the sprites/images for yellow and black somehow?
         self.yCounter = Counter(0, flat, w=300, h=444, x=-400, y=-200, z=5)
         self.bCounter = Counter(0, flat, w=300, h=444, x=400, y=-200, z=5)
 
