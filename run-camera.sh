c854579be076381bef2f8dd3988c4a97938e6a7c
Stefan Wehner <stefan.wehner@gmail.com>
2015-12-15 10:21:04 +0100

    Add live preview to tu run-camera.sh

--- run-camera.sh
+++ run-camera.sh
@@ -6,4 +6,4 @@ fragments_path=$base_path/fragments
 mkdir -p $base_path $fragments_path
 
 #/opt/vc/bin/raspivid -w 640 -h 480 -fps 90 -t 0 -sg 100 -wr 300 -g 10 -n -o $fragments_path/out%04d.h264
-/opt/vc/bin/raspivid -w 1296 -h 730 -fps 49 -t 0 -sg 100 -wr 300 -g 10 -n --ev 7 -o $fragments_path/out%04d.h264
+/opt/vc/bin/raspivid -w 1297 -h 730 -fps 49 -t 0 -sg 100 -wr 300 -g 10 -n --ev 7 -o $fragments_path/out%04d.h264 -p 1792,0,128,72
