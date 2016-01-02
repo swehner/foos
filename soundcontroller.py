78ca4f07563628c66aff859108762dff7f84c2b8
Stefan Wehner <stefan.wehner@gmail.com>
2016-01-02 16:12:17 +0100

    Remove unused images. Fix 'prepare' sound bug when goal overflows counter

--- soundcontroller.py
+++ soundcontroller.py
@@ -5,19 +5,19 @@ import threading
 import subprocess
 from gl.foos_gui import GuiState
 
+
 class SoundController:
     # This map scores => sounds
     sounds = {
         (3, 0): 'perfect',
         (5, 0): 'humiliation',
-        (0, 0): 'prepare',
         (4, 4): '1_frag'
     }
 
     def __init__(self, bus):
         self.bus = bus
         self.bus.subscribe(self.process_event, thread=True)
-        
+
     def process_event(self, ev):
         if ev.name == 'score_goal':
             score = (ev.data['yellow'], ev.data['black'])
@@ -25,13 +25,14 @@ class SoundController:
                 score = score[::-1]
                 if score not in self.sounds:
                     return
-                
+
+            sound = self.sounds[score]
+
         elif ev.name == 'score_reset':
-            score = (0,0)
+            sound = 'prepare'
         else:
             return
-            
-        sound = self.sounds[score]
+
         path = "sounds/{}.wav".format(sound)
         subprocess.call(['play', '-V0', '-G', '-q', path])
 
