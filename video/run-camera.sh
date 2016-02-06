3d237ca27885eab22df9b7940fce860bc9db374a
Stefan Wehner <stefan.wehner@gmail.com>
2016-02-06 10:50:14 +0100

    Make raspivid write motion vector info

--- video/run-camera.sh
+++ video/run-camera.sh
@@ -4,4 +4,4 @@
 
 mkdir -p $base_path $fragments_path
 
-/opt/vc/bin/raspivid -w 1296 -h 730 -fps 49 -t 0 -sg 100 -wr 300 -g 10 --ev 7 -o $fragments_path/out%04d.h264 -p 0,0,128,72
+/opt/vc/bin/raspivid -w 1296 -h 730 -fps 49 -t 0 -sg 100 -wr 300 -g 10 --ev 7 -o $fragments_path/out%04d.h264 -p 0,0,128,72 -x $fragments_path/mv%04d.txt
