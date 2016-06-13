8a82460e79e8bdea87979c8686e2d45894ef5465
Stefan Wehner <stefan.wehner@gmail.com>
2016-06-13 12:04:49 +0200

    Remove config to enable/disable replays

--- plugins/replay.py
+++ plugins/replay.py
@@ -13,10 +13,9 @@ class Plugin:
     def replay(self, replay_type, trigger, extra={}):
         extra['type'] = trigger
 
-        if config.replay_enabled:
-            call(["video/generate-replay.sh", config.replay_path,
-                  str(config.ignore_recent_chunks),
-                  str(config.long_chunks), str(config.short_chunks)])
-            self.bus.notify('replay_start', extra)
-            call(["video/replay.sh", os.path.join(config.replay_path, "replay_{}.h264".format(replay_type))])
-            self.bus.notify('replay_end')
+        call(["video/generate-replay.sh", config.replay_path,
+              str(config.ignore_recent_chunks),
+              str(config.long_chunks), str(config.short_chunks)])
+        self.bus.notify('replay_start', extra)
+        call(["video/replay.sh", os.path.join(config.replay_path, "replay_{}.h264".format(replay_type))])
+        self.bus.notify('replay_end')
