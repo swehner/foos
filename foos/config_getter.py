5531f23d39aecc65c1e721053f192bd04b0294f9
Stefan Wehner <stefan.wehner@gmail.com>
2016-08-05 17:43:27 +0200

    Make video size configurable
    Calculate MD size based on video size
    Default to 1280x720

--- foos/config_getter.py
+++ foos/config_getter.py
@@ -1,5 +1,14 @@
 import config
 import sys
+import collections
+
+def toString(value):
+    if isinstance(value, collections.Iterable) and not isinstance(value, str):
+        return(" ".join(map(toString, value)))
+    else:
+        return str(value)
 
 if __name__ == "__main__":
-    print(getattr(config, sys.argv[1]))
+    value = getattr(config, sys.argv[1])
+
+    print(toString(value))
