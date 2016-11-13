4cb2c62e026cae8762d16e8f4244ee3a8a60453d
Stefan Wehner <stefan.wehner@gmail.com>
2016-11-13 09:46:16 +0000

    Fix errors in bg code & add section on troubleshooting

--- doc/Troubleshooting.md
+++ doc/Troubleshooting.md
@@ -7,8 +7,21 @@ https://www.raspberrypi.org/help/faqs/#power
 
 ### When the replay is supposed to show nothing appears on screen and the score counters don't go back to their original position
 
-This is most likely because you don't have enoguh RAM assigned to your GPU. We recommend at least 192MB. Use raspi-config to change it. 
+This is most likely because you don't have enough RAM assigned to your GPU. See next question.
 
 ### Some UI elements are not drawn correctly and show as gray/black boxes instead
 
-This is most likely because you don't have enoguh RAM assigned to your GPU. We recommend at least 192MB. Use raspi-config to change it. 
+This is most likely because you don't have enoguh RAM assigned to your GPU. We recommend 256MB. Use raspi-config to change it.
+
+### The player blacks out after a few seconds or video shows tearing
+
+This could be related to issues with dispmanx compositing.
+Try disabling the camera preview in config.py:
+```
+camera_preview="-n"
+```
+Or enabling offline compositing in dispmanx in /boo/config.txt (reboot required!):
+```
+dispmanx_offline=1
+```
+
--- foos/ui/bg.py
+++ foos/ui/bg.py
@@ -15,8 +15,8 @@ class UpdatingBGDisplay():
     def __init__(self, imgw, imgh, layer):
         self.layer = layer
         self.w, self.h = ctypes.c_int(), ctypes.c_int()
-        self.imgw, self.imgh = imgw, imgh
-
+        # choose the next smalles multiple of 16 for width and height (might some of the bg)
+        self.imgw, self.imgh = self._alignDown(imgw, 16), self._alignDown(imgh, 16)
         s = bcm.graphics_get_display_size(0, ctypes.byref(self.w), ctypes.byref(self.h)) 
         assert s >= 0
         
@@ -36,21 +36,22 @@ class UpdatingBGDisplay():
         
     @contextmanager
     def bcmUpdate(self):
-        dispman_update = bcm.vc_dispmanx_update_start(0)
+        dispman_update = bcm.vc_dispmanx_update_start(10)
         assert dispman_update != 0
         yield dispman_update
         result = bcm.vc_dispmanx_update_submit_sync(dispman_update);
         assert result == 0
 
     def _alignUp(self, n, alignTo):
-        add = 0 if n % alignTo == 0 else 1
-        return ((n // alignTo) + add) * alignTo
+        return ((n + alignTo - 1) & ~(alignTo - 1))
+
+    def _alignDown(self, n, alignTo):
+        return n & ~(alignTo - 1)
 
     def createBuffer(self):
-        vc_image_ptr = ctypes.c_uint()
-        alignedWidth = self._alignUp(self.imgw, 16)
-        self.pitch = alignedWidth * 3;
-        alignedHeight = self._alignUp(self.imgh, 16)
+        self.vc_image_ptr = ctypes.c_uint()
+        self.pitch = self.imgw * 3;
+        assert(self.pitch % 32 == 0)
         self.imgtype = VC_IMAGE_RGB888
 
         with self.bcmUpdate() as dispman_update:
@@ -58,22 +59,23 @@ class UpdatingBGDisplay():
                 self.imgtype,
                 self.imgw,
                 self.imgh,
-                ctypes.byref(vc_image_ptr))
+                ctypes.byref(self.vc_image_ptr))
             assert (self.img_resource != 0)
-            src_rect = c_ints((0, 0, self.imgw << 16, self.imgh << 16))
+            self.src_rect = c_ints((0, 0, self.imgw << 16, self.imgh << 16))
+            # scale width so that aspect ratio is retained with display height - might crop right part of the bg
             dstw = int(self.h.value * self.imgw / self.imgh)
-            dst_rect = c_ints((0, 0, dstw, self.h))
+            self.dst_rect = c_ints((0, 0, dstw, self.h))
 
-            alpha = c_ints((DISPMANX_FLAGS_ALPHA_FROM_SOURCE, 255, 0))
+            self.alpha = c_ints((DISPMANX_FLAGS_ALPHA_FROM_SOURCE, 255, 0))
 
             self.img_element = bcm.vc_dispmanx_element_add(dispman_update,
                                                     self.dispman_display,
                                                     self.layer,
-                                                    ctypes.byref(dst_rect),
+                                                    ctypes.byref(self.dst_rect),
                                                     self.img_resource,
-                                                    ctypes.byref(src_rect),
+                                                    ctypes.byref(self.src_rect),
                                                     DISPMANX_PROTECTION_NONE,
-                                                    ctypes.byref(alpha),
+                                                    ctypes.byref(self.alpha),
                                                     0,
                                                     DISPMANX_NO_ROTATE)
             assert(self.img_element != 0)
@@ -82,14 +84,16 @@ class UpdatingBGDisplay():
     def loadImg(self, path):
         image = Image.open(path)
 
-        if image.size != (self.imgw, self.imgh):
-            print("Image {} is not of correct size: expected {} but got {}".format(path, (self.imgw, self.imgh), image.size)) 
+        if image.size[0] < self.imgw or image.size[1] < self.imgh:
+            # if the image is too smal we'd have to change the element, so rescale
+            # this shouldn't happen as all bgs are expected to be of a fixed size
+            print("Image {} is too small: expected at least {} but got {} - will resize image".format(path, (self.imgw, self.imgh), image.size))
             image = image.resize((self.imgw, self.imgh), Image.BILINEAR)
 
         if image.mode != "RGB":
             print("Image {} is not of correct mode: expected {} but got {}".format(path, "RGB", image.mode)) 
             image = image.convert("RGB")
-            
+
         return image
         
     def setImg(self, path):
@@ -102,11 +106,9 @@ class UpdatingBGDisplay():
                                                      self.imgtype,
                                                      self.pitch,
                                                      imgbuffer.ctypes.data,
-                                                     ctypes.pointer(bmpRect))
-        dst_rect = c_ints((0, 0, self.w, self.h))
-        
+                                                     ctypes.byref(bmpRect))
         with self.bcmUpdate() as dispman_update:
-            bcm.vc_dispmanx_element_modified(dispman_update, self.img_element, dst_rect);
+            bcm.vc_dispmanx_element_modified(dispman_update, self.img_element, ctypes.byref(self.dst_rect));
 
 
 class BGRotater:
